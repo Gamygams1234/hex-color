8307e43fac9fd8edf5ff86ea70591ab5b001bcd2
Gamaliel Burgos <gamyburgos@gmail.com>
2021-12-17 09:51:06 -0800

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,71 @@
+"use strict";
+let correctHexCode;
+let allHexCodes;
+// generates the correct hexcode, then adds it in a random
+// spot into the "allHexCodes" array which is also random ones
+setup();
+/*
+    your job: create a div for each element in the
+    "allHexCodes" array and set its color to the
+    hexCode. this means that only one div will have
+    the correct hexcode and the others will be random
+    colors.
+    as you create them, add an event listener to each
+    div which will call "incorrectAnswer" if the color
+    is not the correct one, and "correctAnswer" if it is.
+    ALSO, make sure to append each div to the "guesses"
+    div that is in the HTML.
+*/
+function createDivs() {
+  let guesses = document.getElementById("guesses");
+  allHexCodes.forEach(function (code) {
+    guesses.innerHTML += `<div id="${code}" style="background:${code}" class="guess">&nbsp;</div>`;
+  });
+  addListeners();
+}
+
+function addListeners() {
+    document.querySelectorAll('.guess').forEach(item => {
+        item.addEventListener('click', e => {
+            if (e.target.getAttribute("id") === correctHexCode) {
+                correctAnswer();
+              }else{
+                incorrectAnswer();
+              }
+        })
+      })
+}
+
+function setup() {
+  let generatedHexCode = document.getElementById("generated-hexcode");
+  guesses.innerHTML ='';
+  correctHexCode = generateNewHexCode();
+  generatedHexCode.innerText = correctHexCode;
+  allHexCodes = [];
+  let correctIndex = Math.floor(Math.random() * 5);
+  allHexCodes[correctIndex] = correctHexCode;
+  for (let i = 0; i < 5; i++) {
+    if (i !== correctIndex) {
+      allHexCodes[i] = generateNewHexCode();
+    }
+  }
+  createDivs();
+}
+function generateNewHexCode() {
+  let hexcode = "#";
+  for (let i = 0; i < 6; i++) {
+    let letterOrNumber = Math.floor(Math.random() * 2);
+    if (letterOrNumber === 0) hexcode += "" + Math.floor(Math.random() * 10);
+    else {
+      hexcode += "" + String.fromCharCode(Math.floor(Math.random() * 6) + 65);
+    }
+  }
+  return hexcode;
+}
+function incorrectAnswer() {
+  alert("Sorry, that's not the right one! Try again.");
+}
+function correctAnswer() {
+  alert("You got it! Nice job!");
+  setup();
+}
